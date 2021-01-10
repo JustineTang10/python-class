334dda8c8aa81d0a1ecfda444647627ff15a54b8
JustineTang10 <68197279+JustineTang10@users.noreply.github.com>
2021-01-10 16:15:03 -0500

    Update 1-10-2021.py

--- 1-10-2021.py
+++ 1-10-2021.py
@@ -18,3 +18,29 @@ def secret_number():
       numberoftries += 1
   tryword = "try" if numberoftries == 1 else "tries"
   print("\nIt took you",numberoftries,tryword,"to guess the number")
+
+ # Version WITH recursion
+import random
+
+def secret_number2(goal, tries):
+  chosennumber = int(input("What number (from 1-50) would you like to choose? "))
+  won = False
+  if chosennumber > 50 or chosennumber < 1:
+    print("Invalid option, try again")
+  elif chosennumber == goal:
+    print("That's the number, you win!")
+    tries += 1
+    won = True
+  elif chosennumber < goal:
+    print("That number is too low")
+    tries += 1
+  else:
+    print("That number is too high")
+    tries += 1
+  if won:
+    tryword = "try" if tries == 1 else "tries"
+    print("\nIt took you",tries,tryword,"to guess the number")
+  else:
+    secret_number2(goal, tries)
+
+secret_number2(random.randint(1, 50), 0)
