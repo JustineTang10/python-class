807c1c6eabedc7fd6645026c2c8f454ef34d7a25
JustineTang10 <68197279+JustineTang10@users.noreply.github.com>
2021-02-21 11:16:08 -0500

    Create 2-21-2021.py

---
+++ 2-21-2021.py
@@ -0,0 +1,9 @@
+def feb_21():
+  # Homework 1
+  string = 'Justine'
+  print(f'Length: {len(string)}\nLast letter: {string[-1]}')
+
+  # Homework 2
+  breakfast = 'I had oatmeal for breakfast'
+  lunch = breakfast.replace('oatmeal', 'pasta').replace('breakfast', 'lunch')
+  print(lunch)
